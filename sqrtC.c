c8b7cb5f21e5416d442f19fcbe98d02f13bc2c9c
Nowacki.Maciej <209408@edu.p.lodz.pl>
2019-05-13 15:07:10 +0200

    Fixes

--- sqrtC.c
+++ sqrtC.c
@@ -1,5 +1,3 @@
-/*  Make the necessary includes and set up the variables.  */
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -7,49 +5,102 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-int
-main ()
+int isBigEndian() 
 {
-	int sockfd;
-	socklen_t len;
-	struct sockaddr_in address;
-	double number;
-	int length;
-	scanf("%lf", &number);
+	int x = 1;
+	//1000 0000 - littleEndian
+	//0000 0001 - bigEndian
+	if((int) (((char *)&x)[0]) == 1)
+		return 0; //littleEndian
+	else
+		return 1; //bigEndian
+}
 
+int main()
+{
+	srand(time(0)); //inicjalizacja ziarna dla random()
+	int sockfd; //deskryptor polaczenia
+	socklen_t len; //rozmiar struktury; potrzebne do polaczenia
+	struct sockaddr_in address;
+
+	int result; //rezultat polaczenia; 0 - polacznie udane, -1 - polacznie nieudane
+	double liczba; //pierw przechowuje liczbe DO pierwiastkowania, pozniej PO pierwiastkowaniu
+	int length; //dlugosc stringa daty i czasu; potrzebne do zaalokowania pamieci
+
+	char str[32]; //tymczasowy buffor dla liczby w formacie stringa; potrzebne dla wyznaczenia pozycji korpki
+	char *whereIsTheDot;
+	
+	int rqId;
+	int rqIdReturnedByServer;
+	int status; //walidacja wprowadzonej liczby
+	char *buffer = NULL; //buffor dla wprowadzonego tekstu; potrzebne do sscanf
+	size_t n = 0; //wielkosc przechwyconego inputa, to samo co unsigned int
+	char choose = 'T';
+	printf("You're %s-Endian\n", isBigEndian() ? "Big" : "Little");
+	
 	/*  Create a socket for the client.  */
 
-	sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM - TCP
 
 	/*  Name the socket, as agreed with the server.  */
 
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr ("127.0.0.1");
-	address.sin_port = htons (9734);
-	len = sizeof (address);
+	address.sin_addr.s_addr = inet_addr("127.0.0.1"); //AF_INET - IPv4
+	address.sin_port = htons(9734); //host to network short
+	len = sizeof(address);
 
 	/*  Now connect our socket to the server's socket.  */
 
-	result = connect (sockfd, (struct sockaddr *) &address, len);
+	result = connect(sockfd, (struct sockaddr *) &address, len);
 
-	if (result == -1)
+	if(result == -1)
 	{
-		perror ("oops: netclient");
+		perror("oops: netclient");
 		exit (1);
 	}
 
 	/*  We can now read/write via sockfd.  */
+	
+	do
+	{
+		rqId = 1 + rand() % 10000000;
+		
+		do
+		{
+			getline(&buffer, &n, stdin);
+	   		status = sscanf(buffer, "%lf", &liczba);
+		}
+		while(liczba < 0 || status != 1);
 
-	write (sockfd, &number, 8);
-	read (sockfd, &number, 8); 
+		snprintf(str, sizeof(str), "%lf", liczba);
+		whereIsTheDot = strchr(str, '.');
+		printf("Dot position: %ld\n", whereIsTheDot-str);
 
-	read (sockfd, &length, 4); 
-	char * date = (char*) malloc (length * sizeof(char));
-	read (sockfd, date, length);
+		write(sockfd, &rqId, 4);
+		write(sockfd, &liczba, 8);
 
- 	printf ("The square root of this number is %f\n", number);
-	printf("Current date: %s\n", date);
+		read(sockfd, &rqIdReturnedByServer, 4);
+		printf("Sent ID: %d, Returned ID: %d\n", rqId, rqIdReturnedByServer);
+
+		read(sockfd, &liczba, 8); 
+		read(sockfd, &length, 4); 
+
+		char * data = (char*) malloc (length * sizeof(char));
+		//char data[length+1];
+
+		read (sockfd, data, length);
+		data[length] = '\0';
+
+	 	printf("Result: %f\n", liczba);
+		printf("Date: %s\n", data);
+		printf("Do you want to continue?? [Y/N]: ");
+		scanf(" %c", &choose);
+		write(sockfd, &choose, 1);
+	}while(choose != 'N');
 
 	close (sockfd);
 	exit (0);
--- sqrtS.c
+++ sqrtS.c
@@ -9,65 +9,81 @@
 #include <string.h>
 #include <time.h>
 
-int
-main ()
+int main()
 {
 	int server_sockfd, client_sockfd;
 	socklen_t server_len, client_len;
 	struct sockaddr_in server_address;
 	struct sockaddr_in client_address;
 
-	server_sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	server_sockfd = socket(AF_INET, SOCK_STREAM, 0); //AF_INET - IPv4
 
 	server_address.sin_family = AF_INET;
-	server_address.sin_addr.s_addr = htonl (INADDR_ANY);
-	server_address.sin_port = htons (9734);
-	server_len = sizeof (server_address);
-	bind (server_sockfd, (struct sockaddr *) &server_address, server_len);
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY); //bindujemy WSZYSTKIE interfejsy, nie tylko np localhost
+	server_address.sin_port = htons(9734);
+	server_len = sizeof(server_address);
+	bind(server_sockfd, (struct sockaddr *) &server_address, server_len); //powiazanie socketa z deskryptorem, domyslnie gniazdo jest anonimowe
 
 	/*  Create a connection queue, ignore child exit details and wait for clients.  */
 
-	listen (server_sockfd, 5);
+	listen(server_sockfd, 5); //5 - dlugosc kolejki w oczekiwaniu na sockfd
 
-	signal (SIGCHLD, SIG_IGN);
+	signal(SIGCHLD, SIG_IGN); //pozwala zamknac sie procesom zombie 
 
 	while (1)
 	{
 		
-		printf ("Server waiting\n");
+		printf("Server waiting for new clients\n");
 		
 		/*  Accept connection.  */
 
-		client_len = sizeof (client_address);
-		client_sockfd = accept (server_sockfd,
-				(struct sockaddr *) &client_address,
-				&client_len);
+		client_len = sizeof(client_address);
+		client_sockfd = accept(server_sockfd, (struct sockaddr *) &client_address, &client_len);
+		printf("Client connected\n");
+		fflush(stdout);
 
 		/*  Fork to create a process for this client */
 
-		if (fork () == 0)
+		if(fork() == 0)
 		{
-			double number;
-			time_t mytime = time(NULL);
-			char * time_str = ctime(&mytime);
-			time_str[strlen(time_str)-1] = '\0';
-			int length = strlen(time_str);
-
-			read (client_sockfd, &number, 8);
-			number = sqrt(number);
-			write (client_sockfd, &number, 8);
+			double liczba;
+			int rqId;
+			char choose;
+			time_t mytime;
+			char * time_str;
 			
-			write(client_sockfd, &length, 4);
-			write(client_sockfd, time_str, length);
-			close (client_sockfd);
-			exit (0);
+			do
+			{
+				mytime = time(NULL);
+				time_str = ctime(&mytime);
+				//time_str[strlen(time_str)-1] = '\0';
+				int length = strlen(time_str);
+
+				read(client_sockfd, &rqId, 4);
+				read(client_sockfd, &liczba, 8);
+
+				printf("SQRT of %lf is %lf. RequestId: %d\n", liczba, sqrt(liczba), rqId);
+				fflush(stdout);
+
+				liczba = sqrt(liczba);
+				write(client_sockfd, &rqId, 4);
+				write(client_sockfd, &liczba, 8);
+				write(client_sockfd, &length, 4);
+				write(client_sockfd, time_str, length-1);
+
+				read(client_sockfd, &choose, 1);
+			}while(choose != 'N');
+			close(client_sockfd);
+			printf("Client disconnected\n");
+			fflush(stdout);
+			exit(0);
 		}
 
 		/*  Otherwise, we must be the parent and our work for this client is finished.  */
 
 		else
 		{
-			close (client_sockfd);
+			close(client_sockfd);
 		}
 	}
 }
